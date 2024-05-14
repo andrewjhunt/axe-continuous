6a33cd67a5af8d24174149476bb8582714cf1bf1
Andrew Hunt <andrewjhunt@users.noreply.github.com>
2024-05-15 08:50:02 +1000

    fix script url, add progress events

--- axe-continuous-simple.js
+++ axe-continuous-simple.js
@@ -1,13 +1,17 @@
 // Single JavaScript file that sets up axe-continuous with report output to console
 
 function loadScript(url, onloadFunc) {
+  console.log(`axe-continuous-simple: loading ${url}`)
   const scriptEl = document.createElement("script")
   document.body.append(scriptEl)
 
   scriptEl.onerror = (err) => {
     throw new URIError(`The script ${err.target.src} didn't load correctly.`)
   }
-  if (onloadFunc) scriptEl.onload = onloadFunc
+  if (onloadFunc) scriptEl.onload = () => {
+    console.log(`axe-continuous-simple: load successful ${url}`)
+    onloadFunc()
+  }
 
   scriptEl.src = url
 }
@@ -16,8 +20,7 @@ loadScript(
   "https://cdn.jsdelivr.net/npm/axe-core@4.9.1/axe.min.js", 
   () => {
     loadScript(
-      "./axe-continuous.js",
-      // "https://www.musios.app/axe-continuous/axe-continuous.js",
+      "https://www.musios.app/axe-continuous/axe-continuous.js",
       () => {
         axeContinuous.start()
         console.log("axeContinuous is running")
